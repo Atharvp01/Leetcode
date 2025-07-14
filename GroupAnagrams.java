3c44837f32de68743a0230ccdbda5bf872729d09
Atharv Patil <atharvpatil5177@gmail.com>
2025-07-14 11:14:22 +0530

    Group Anagrams (efficient)

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -1,43 +1,28 @@
 // https://leetcode.com/problems/group-anagrams/
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class GroupAnagrams {
   public static List<List<String>> groupAnagrams(String[] strs) {
-    List<List<String>> listAnagrams = new ArrayList<>();
-    boolean[] used = new boolean[strs.length];
-    for (int i = 0; i < strs.length; i++) {
-      List<String> anagrams = new ArrayList<>();
-      String str1 = strs[i];
-      if (used[i]) continue;
-      anagrams.add(str1);
-      used[i] = true;
-      for (int j = i + 1; j < strs.length; j++) {
-        String str2 = strs[j];
-        if (str1.length() != str2.length()) continue;
-        Boolean isAnagram = true;
-        if (used[j]) continue;
-        int[] arr = new int[26];
-        for (int k = 0; k < str1.length(); k++) {
-          arr[str1.charAt(k) - 'a']++;
-          arr[str2.charAt(k) - 'a']--;
-        }
-        for (int num : arr) {
-          if (num == 0) continue;
-          else {
-            isAnagram = !isAnagram;
-            break;
-          }
-        }
-        if (isAnagram) {
-          anagrams.add(str2);
-          used[j] = true;
-        }
+
+    Map<String, List<String>> map = new HashMap<>();
+
+    for (String str : strs) {
+      char[] charArray = str.toCharArray();
+      Arrays.sort(charArray);
+      String sortedStr = new String(charArray);
+
+      if (!map.containsKey(sortedStr)) {
+        map.put(sortedStr, new ArrayList<>());
       }
-      listAnagrams.add(anagrams);
+      map.get(sortedStr).add(str);
     }
-    return listAnagrams;
+
+    return new ArrayList<>(map.values());
   }
 
   public static void main(String[] args) {
