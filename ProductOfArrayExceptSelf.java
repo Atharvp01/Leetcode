74d4d0899b6139e52eeee1f2d1baf59b51886b59
Atharv Patil <atharvpatil5177@gmail.com>
2025-09-20 13:14:46 +0530

    update

---
+++ ProductOfArrayExceptSelf.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/product-of-array-except-self/description/
+
+public class ProductOfArrayExceptSelf {
+
+  public static int[] productExceptSelf(int[] nums) {}
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {1, 2, 3, 4};
+    int[] res = productExceptSelf(nums);
+    System.out.println(Arrays.toString(res));
+  }
+}
